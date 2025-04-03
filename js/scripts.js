e342068baae38f553f356aeb761ad00bdda25f7b
caamomilaa <c.velazquezdamico@gmail.com>
2025-04-03 23:14:34 +0200

    update

--- js/scripts.js
+++ js/scripts.js
@@ -19,62 +19,108 @@ const symbols = '!@#$%^&*()_+-={}[]:;<>,.?/';
 //LÓGICA DE LA CONTRSEÑA
 
 const getPasswordLength = () => {
-  passwordLengthElement.textContent = rangeInputElement.value;
-  return getPasswordLength;
+	passwordLengthElement.textContent = rangeInputElement.value;
+	return getPasswordLength;
 };
 
 const selectedOptions = () => {
-  let selectedCharacters = '';
-  if (uppercaseInputElement.checked) {
-    selectedCharacters += uppercaseCharacters;
-  }
-  if (lowerercaseInputElement.checked) {
-    selectedCharacters += lowercaseCharacters;
-  }
-  if (numbersInputElement.checked) {
-    selectedCharacters += numbers;
-  }
-  if (symbolsInputElement.checked) {
-    selectedCharacters += symbols;
-  }
+	let selectedCharacters = '';
+	if (uppercaseInputElement.checked) {
+		const randomPosition = Math.floor(
+			Math.random() * uppercaseCharacters.length
+		);
+		const randomCharacter = uppercaseCharacters.charAt(randomPosition);
+		selectedCharacters += randomCharacter;
+	}
+	if (lowerercaseInputElement.checked) {
+		const randomPosition = Math.floor(
+			Math.random() * lowercaseCharacters.length
+		);
+		const randomCharacter = lowercaseCharacters.charAt(randomPosition);
+		selectedCharacters += randomCharacter;
+	}
+	if (numbersInputElement.checked) {
+		const randomPosition = Math.floor(Math.random() * numbers.length);
+		const randomCharacter = numbers.charAt(randomPosition);
+		selectedCharacters += randomCharacter;
+	}
+	if (symbolsInputElement.checked) {
+		const randomPosition = Math.floor(Math.random() * symbols.length);
+		const randomCharacter = symbols.charAt(randomPosition);
+		selectedCharacters += randomCharacter;
+	}
 
-  //AL MENOS UNO DE ELLOS
+	//AL MENOS UNO DE ELLOS
 
-  activateButton();
-  return selectedCharacters;
+	activateButton();
+	return selectedCharacters;
 };
+// ****PRUEBA****
 
+// const getRandom = () => {
+// 	let charactersThatIget = '';
+// 	if (10 % 2 === 0) {
+// 		//quiero que me selecciones una aleatoria
+// 		const randomPosition = Math.floor(
+// 			Math.random() * uppercaseCharacters.length
+// 		);
+// 		const randomCharacter = uppercaseCharacters.charAt(randomPosition);
+// 		charactersThatIget += randomCharacter;
+// 	}
+
+// 	console.log(charactersThatIget);
+// 	return charactersThatIget;
+// };
+
+// const generatePassword = () => {
+// 	const rangeBar = rangeInputElement.value;
+
+// 	let randomPasswordCharacters = charactersThatIget;
+// 	let totalRandomCharacters = '';
+
+// 	for (i = 0; i < rangeBar; i++) {
+// 		const randomPosition = Math.floor(
+// 			Math.random() * randomPasswordCharacters.length
+// 		);
+// 		const randomCharacter = randomPasswordCharacters.charAt(randomPosition);
+// 		totalRandomCharacters += randomCharacter;
+// 	}
+// 	return totalRandomCharacters;
+// };
+
+// ****PRUEBA****
 const generatePassword = () => {
-  const rangeBar = rangeInputElement.value;
+	const rangeBar = rangeInputElement.value;
 
-  let randomPasswordCharacters = selectedOptions();
-  let totalRandomCharacters = '';
+	let randomPasswordCharacters = selectedOptions();
+	let totalRandomCharacters = '';
 
-  for (i = 0; i < rangeBar; i++) {
-    const randomPosition = Math.floor(
-      Math.random() * randomPasswordCharacters.length
-    );
-    const randomCharacter = randomPasswordCharacters.charAt(randomPosition);
-    totalRandomCharacters += randomCharacter;
-  }
-  return totalRandomCharacters;
+	for (i = 0; i < rangeBar; i++) {
+		const randomPosition = Math.floor(
+			Math.random() * randomPasswordCharacters.length
+		);
+		const randomCharacter = randomPasswordCharacters.charAt(randomPosition);
+		totalRandomCharacters += randomCharacter;
+	}
+	return totalRandomCharacters;
 };
 
 const finalPassword = () => {
-  passwordElement.value = generatePassword();
+	passwordElement.value = generatePassword();
 };
 
 const activateButton = () => {
-  if (
-    uppercaseInputElement.checked ||
-    lowerercaseInputElement.checked ||
-    numbersInputElement.checked ||
-    symbolsInputElement.checked
-  ) {
-    generatorButtonElement.disabled = false;
-  } else {
-    generatorButtonElement.disabled = true;
-  }
+	if (
+		uppercaseInputElement.checked ||
+		lowerercaseInputElement.checked ||
+		numbersInputElement.checked ||
+		symbolsInputElement.checked
+	) {
+		generatorButtonElement.disabled = false;
+	} else {
+		generatorButtonElement.disabled = true;
+		passwordElement.value = 'P4$5W0RD!';
+	}
 };
 
 rangeInputElement.addEventListener('input', getPasswordLength);
